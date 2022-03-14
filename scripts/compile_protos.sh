b4e465a4a9667fc8bd586322d0a97f2601a5185c
mjb <472812069@qq.com>
2022-03-14 11:30:57 +0800

    修改compile文件

--- scripts/compile_protos.sh
+++ scripts/compile_protos.sh
@@ -16,20 +16,21 @@ set -eux
 PROTO_ROOT_FILES="$(find . -name ".protoroot" -exec readlink -f {} \;)"
 PROTO_ROOT_DIRS="$(dirname $PROTO_ROOT_FILES)"
 
+echo $PROTO_ROOT_DIRS
 
-# Find all proto files to be compiled, excluding any which are in a proto root or in the vendor folder
-ROOTLESS_PROTO_FILES="$(find $PWD \
-                             $(for dir in $PROTO_ROOT_DIRS ; do echo "-path $dir -prune -o " ; done) \
-                             -path $PWD/vendor -prune -o \
-                             -path $PWD/.build -prune -o \
-                             -path $PWD/core/chaincode/shim/java -prune -o \
-                             -name "*.proto" -exec readlink -f {} \;)"
-ROOTLESS_PROTO_DIRS="$(dirname $ROOTLESS_PROTO_FILES | sort | uniq)"
+# # Find all proto files to be compiled, excluding any which are in a proto root or in the vendor folder
+# ROOTLESS_PROTO_FILES="$(find $PWD \
+#                              $(for dir in $PROTO_ROOT_DIRS ; do echo "-path $dir -prune -o " ; done) \
+#                              -path $PWD/vendor -prune -o \
+#                              -path $PWD/.build -prune -o \
+#                              -path $PWD/core/chaincode/shim/java -prune -o \
+#                              -name "*.proto" -exec readlink -f {} \;)"
+# ROOTLESS_PROTO_DIRS="$(dirname $ROOTLESS_PROTO_FILES | sort | uniq)"
 
-for dir in $ROOTLESS_PROTO_DIRS $PROTO_ROOT_DIRS; do
-echo Working on dir $dir
-        # As this is a proto root, and there may be subdirectories with protos, compile the protos for each sub-directory which contains them
-	for protos in $(find "$dir" -name '*.proto' -exec dirname {} \; | sort | uniq) ; do
-	       protoc --proto_path="$dir" --go_out=plugins=grpc:$GOPATH/src "$protos"/*.proto
-	done
-done
+# for dir in $ROOTLESS_PROTO_DIRS $PROTO_ROOT_DIRS; do
+# echo Working on dir $dir
+#         # As this is a proto root, and there may be subdirectories with protos, compile the protos for each sub-directory which contains them
+# 	for protos in $(find "$dir" -name '*.proto' -exec dirname {} \; | sort | uniq) ; do
+# 	       protoc --proto_path="$dir" --go_out=plugins=grpc:$GOPATH/src "$protos"/*.proto
+# 	done
+# done
