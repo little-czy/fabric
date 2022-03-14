a772e7308948b2e7624c5c3d5b8c3291eae057f6
mjb <472812069@qq.com>
2022-03-14 15:47:20 +0800

    修改compile文件

--- compile_protos.sh
+++ compile_protos.sh
@@ -27,7 +27,7 @@ echo $PROTO_ROOT_DIRS
 #                              -path $PWD/.build -prune -o \
 #                              -path $PWD/core/chaincode/shim/java -prune -o \
 #                              -name "*.proto";)"
-ROOTLESS_PROTO_DIRS="$(dirname $ROOTLESS_PROTO_FILES | sort | uniq)"
+ROOTLESS_PROTO_DIRS=$PROTO_ROOT_FILES
 
 # # Find all proto files to be compiled, excluding any which are in a proto root or in the vendor folder
 # ROOTLESS_PROTO_FILES="$(find $PWD \
@@ -38,10 +38,10 @@ ROOTLESS_PROTO_DIRS="$(dirname $ROOTLESS_PROTO_FILES | sort | uniq)"
 #                              -name "*.proto" -exec readlink -f {} \;)"
 # ROOTLESS_PROTO_DIRS="$(dirname $ROOTLESS_PROTO_FILES | sort | uniq)"
 
-# for dir in $ROOTLESS_PROTO_DIRS $PROTO_ROOT_DIRS; do
-# echo Working on dir $dir
-#         # As this is a proto root, and there may be subdirectories with protos, compile the protos for each sub-directory which contains them
-# 	for protos in $(find "$dir" -name '*.proto' -exec dirname {} \; | sort | uniq) ; do
-# 	       protoc --proto_path="$dir" --go_out=plugins=grpc:$GOPATH/src "$protos"/*.proto
-# 	done
-# done
+for dir in $ROOTLESS_PROTO_DIRS $PROTO_ROOT_DIRS; do
+echo Working on dir $dir
+        # As this is a proto root, and there may be subdirectories with protos, compile the protos for each sub-directory which contains them
+	for protos in $(find "$dir" -name '*.proto' -exec dirname {} \; | sort | uniq) ; do
+	       protoc --proto_path="$dir" --go_out=plugins=grpc:$GOPATH/src "$protos"/*.proto
+	done
+done
