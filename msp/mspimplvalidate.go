bc8fc569069a1cb0de0f812f76a472178744c03c
mjb <472812069@qq.com>
2022-05-24 23:40:03 +0800

    打印验证链

--- msp/mspimplvalidate.go
+++ msp/mspimplvalidate.go
@@ -25,7 +25,8 @@ func (msp *bccspmsp) validateIdentity(id *identity) error {
 	}
 
 	// M1.4 显示验证的流程, 打印验证链
-	mspIdentityLogger.Infof("validationChain is %v\n", validationChain)
+	mspIdentityLogger.Infof("validationChain is %v\n", validationChain[0])
+	mspIdentityLogger.Infof("validationChain is %v\n", validationChain[1])
 
 	err = msp.validateIdentityAgainstChain(id, validationChain)
 	if err != nil {
